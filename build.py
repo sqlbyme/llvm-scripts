3559ddcd9bbe6d589b80e854150a0afffc4495e3
Mike Edwards <mike@sqlby.me>
2016-05-13 05:09:17 -0700

    Starting over a bit and getting llvm to work first.

--- build.py
+++ build.py
@@ -3,61 +3,90 @@
 import os
 import shutil
 from subprocess import check_call, check_output
+import sys
 import time
+
+# TODO: Add in some argv processing
+# TODO: Add some default args which cause us to build
+# llvm, clang ad lld by default
+# TODO: Add a docstring to provide help via the argv 
+# processing thingy
+# TODO: Add a function for printing a report header 
+# and a section header
+
+
 
 def build_clang_lld_llvm():
 
+    # TODO: Remove this clear statement and just print a report header instead.
     os.system('clear')
+    # TODO: this is a good example of a place to use a section header
     print 'Starting build timing test.'
 
-    BASE_DIR = os.environ['HOME']
-    # WORKDIR should be changed to suit.
-    WORKDIR = '%s/pg/llvm' % BASE_DIR
-    CLANG_SRC_DIR = '%s/clang.src' % WORKDIR
-    LLD_SRC_DIR = '%s/lld.src' % WORKDIR
-    LLVM_SRC_DIR = '%s/llvm.src' % WORKDIR
-    SRC_PATHS = [CLANG_SRC_DIR, LLD_SRC_DIR, LLVM_SRC_DIR]
-    LLVM_OBJ_DIR = '%s/llvm.obj' % WORKDIR
+    BASE_DIR = '%s/pg' % os.environ['HOME']
+    WORK_DIR = '%s/llvm' % BASE_DIR
+    CLANG_SRC_DIR = '%s/clang.src' % WORK_DIR
+    LLD_SRC_DIR = '%s/lld.src' % WORK_DIR
+    LLVM_SRC_DIR = '%s/llvm.src' % WORK_DIR
+    LLVM_OBJ_DIR = '%s/llvm.obj' % WORK_DIR
+    LLVM_BASE_URL = 'http://llvm.org/git'
+    LLVM_UPSTREAM_URL = '%s/llvm.git' % LLVM_BASE_URL
+    CLANG_UPSTREAM_URL = '%s/clang.git' % LLVM_BASE_URL
+    LLD_UPSTREAM_URL = '%s/lld.git' % LLVM_BASE_URL
 
-    # STEP - Update local repositories
-    for path in SRC_PATHS:
-        os.chdir(path)
+    if not os.path.exists(BASE_DIR):
+        print """This script expects a playground directory named 'pg' 
+            to be located in the users home directory.  Please ensure
+            this has been created prior to continuing."""
+        sys.exit(1)
+
+    if not os.path.exists(WORK_DIR):
+        os.mkdir(WORK_DIR)
+
+    os.chdir(WORK_DIR)
+
+    if not os.path.exists(LLVM_SRC_DIR):
         shell_command = [
             'git',
-	        'pull',
-	        'origin',
-	        'master'
-	    ]
-        check_output(shell_command)
-        shell_command = [
-		    'git',
-			'rev-parse',
-			'HEAD'
-		]
-        git_rev = check_output(shell_command)
-        print '%s was updated to rev:%s' % (path,git_rev)
-
-    if os.path.exists(LLVM_OBJ_DIR):
-	    shutil.rmtree(LLVM_OBJ_DIR, True)
-	    os.mkdir(LLVM_OBJ_DIR)
-	    os.chdir(LLVM_OBJ_DIR)
-	    print 'Removed and re-created llvm.obj dir.'
+            'clone',
+            LLVM_UPSTREAM_URL,
+            LLVM_SRC_DIR
+        ]
+        result = check_call(shell_command)
+        assert (result == 0), 'Git clone of %s failed.' % LLVM_UPSTREAM_URL
     else:
-	    os.mkdir(LLVM_OBJ_DIR)
-	    os.chdir(LLVM_OBJ_DIR)
-	    print 'Created llvm.obj dir.'
+        os.chdir(LLVM_SRC_DIR)
+        shell_command = [
+            'git',
+            'fetch'
+        ]
+        check_call(shell_command)
+        shell_command = [
+            'git',
+            'reset',
+            'FETCH_HEAD'
+        ]
+        result = check_call(shell_command)
+        assert (result == 0), 'Git fetch of llvm failed.'
 
-    print 'Currently working in %s' % os.getcwd()
+
+    os.chdir(WORK_DIR)
+    if os.path.exists(LLVM_OBJ_DIR):
+        shutil.rmtree(LLVM_OBJ_DIR, True)
+
+    os.mkdir(LLVM_OBJ_DIR)
+    os.chdir(LLVM_OBJ_DIR)
 
     # Setup and run cmake for building clang, lld & llvm
     cmake_command = [
 	    'cmake',
+                '-DCMAKE_MAKE_PROGRAM:STRING=/usr/bin/ninja',
 		'-DCMAKE_BUILD_TYPE:STRING=Release',
 		'-DLLVM_ENABLE_ASSERTIONS:BOOL=OFF',
 		'-DLLVM_TARGETS_TO_BUILD:STRING=X86',
 		'-DCMAKE_INSTALL_PREFIX:PATH=%s/Release' % LLVM_OBJ_DIR,
-		'-DLLVM_EXTERNAL_CLANG_SOURCE_DIR:PATH=%s' % CLANG_SRC_DIR,
-		'-DLLVM_EXTERNAL_LLD_SOURCE_DIR:PATH=%s' % LLD_SRC_DIR,
+		#'-DLLVM_EXTERNAL_CLANG_SOURCE_DIR:PATH=%s' % CLANG_SRC_DIR,
+		#'-DLLVM_EXTERNAL_LLD_SOURCE_DIR:PATH=%s' % LLD_SRC_DIR,
 		'-GNinja',
 		LLVM_SRC_DIR
 	]
